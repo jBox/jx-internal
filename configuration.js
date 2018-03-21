9903c92fb6c3023f2d3755e12a141a67fd131914
jBox <junhua@outlook.com>
2018-03-21 23:01:19 +0800

    update

--- configuration.js
+++ configuration.js
@@ -1,18 +1,11 @@
 const cv = require("config-vars");
 
 module.exports = cv.setup((getenv) => ({
-    port: getenv("JX_INTERNAL_PORT"),
-    shareFolder: getenv("JX_INTERNAL_SHARE_FOLDER"),
+    port: getenv("JX_IN_PORT"),
+    shareFolder: getenv("JX_IN_SHARE_FOLDER"),
     jx: {
-        certPath: getenv("JX_INTERNAL_RSA_CERT_PATH"),
+        certPath: getenv("JX_IN_RSA_CERT_PATH"),
         servicePort: getenv("JX_SERVICE_PORT"),
         adminHost: getenv("JX_ADMIN_HOST")
-    },
-    aliyun: {
-        accessKey: getenv("ALI_ACC_ID"),
-        accessSecret: getenv("ALI_ACC_SEC"),
-        sms: {
-            
-        }
     }
 }));
